5e801459509478ebd19d32ce23381135fdd55c7d
Omar <omarabdelraheeem@gmail.com>
2024-07-13 21:44:54 +0300

    create task one

---
+++ quiz.js
@@ -0,0 +1,22 @@
+function checkAnswer() {
+  let correctAnswer = "4";
+  let userAnswer = document.querySelector("input[name='quiz']:checked").value;
+  let feedBack = document.getElementById("feedback");
+  if (correctAnswer == userAnswer) {
+    feedBack.textContent = "Correct! Well done.";
+  } else {
+    feedBack.textContent = "That's incorrect. Try again!";
+  }
+  console.log(userAnswer);
+}
+let submitBtn = document.getElementById("submit-answer");
+submitBtn.addEventListener("click", checkAnswer);
+
+// let correctAnswer = "4";
+// let userAnswer = document.querySelector("input[name='quiz']");
+// let feedBack = document.getElementById("feedback");
+// if (correctAnswer === userAnswer) {
+//   console.log("Correct! Well done.");
+// } else {
+//   console.log("That's incorrect. Try again!");
+// }
